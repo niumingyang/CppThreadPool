32b75ea71e784bb1ee9873a98cc1eb630a69cf01
NiuMingyang <niumingyang@smail.nju.edu.cn>
2024-04-08 17:02:00 +0800

    Initial

---
+++ build/Debug/threadpool/tests/CMakeFiles/threadpool_test.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/d/workspace/CppThreadPool/log/log.cpp" "threadpool/tests/CMakeFiles/threadpool_test.dir/__/__/log/log.cpp.o" "gcc" "threadpool/tests/CMakeFiles/threadpool_test.dir/__/__/log/log.cpp.o.d"
+  "/mnt/d/workspace/CppThreadPool/log/pr.cpp" "threadpool/tests/CMakeFiles/threadpool_test.dir/__/__/log/pr.cpp.o" "gcc" "threadpool/tests/CMakeFiles/threadpool_test.dir/__/__/log/pr.cpp.o.d"
+  "/mnt/d/workspace/CppThreadPool/threadpool/tests/test_threadpool.cpp" "threadpool/tests/CMakeFiles/threadpool_test.dir/test_threadpool.cpp.o" "gcc" "threadpool/tests/CMakeFiles/threadpool_test.dir/test_threadpool.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
